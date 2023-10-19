5c0b183aa5461ecc6cecf149c25bd40fcee27deb
SW ENG <sweng@massload.com>
2023-10-19 16:11:58 -0600

    initial commit- keep source

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TourCard } from "./components/TourCard";
 import Grid from '@mui/material/Grid'; 
 function App() {
   return (
+    // https://www.youtube.com/watch?v=o1chMISeTC0
     <div className="App">
     <Container>
     <Grid container spacing={3}>
